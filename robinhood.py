ae9fae1016265bc9ba09822c2b5d849b89ac1708
Andy Conklin <dandy.andy.conklin@gmail.com>
2016-08-01 23:47:31 -0500

    just fixin up the code a lil

--- robinhood.py
+++ robinhood.py
@@ -14,30 +14,29 @@ class Robinhood:
     def _robin_post(self, endpoint, data={}):
         print "_robin_post called on %s" % (endpoint)
         return requests.post(endpoint if "api.robinhood.com" in endpoint else "https://api.robinhood.com%s" % (endpoint), data=data, headers=self._headers)
+    def _assert_200(self, r):
+        if r.status_code != 200:
+            raise Exception("HTTP %d: %s" % (r.status_code, r.text))
+        return True
     def login(self):
         """Log into Robinhood with the username and password from the constructor"""
         r = self._robin_post("/api-token-auth/", {"username":self._username, "password":self._password})
-        if r.status_code == 200:
-            self._headers["Authorization"] = "Token %s" % (json.loads(r.text)["token"])
-        else:
-            raise Exception("HTTP %d: %s" % (r.status_code, r.text))
+        self._assert_200(r)
+        self._headers["Authorization"] = "Token %s" % (json.loads(r.text)["token"])
         r = self._robin_get("/accounts/")
-        if r.status_code == 200:
-            self._accounts = json.loads(r.text)
-        else:
-            raise Exception("HTTP %d: %s" % (r.status_code, r.text))
+        self._assert_200(r)
+        self._accounts = json.loads(r.text)
         return True
     def logout(self):
         """Log out of Robinhood. Must be logged in."""
         r = self._robin_post("/api-token-logout/", data={})
-        if r.status_code == 200:
-            del self._headers["Authorization"]
-        else:
-            raise Exception("HTTP %d: %s" % (r.status_code, r.text))
+        self._assert_200(r)
+        del self._headers["Authorization"]
         return True
     def get_all_filled_orders(self):
         """Actually only gets the first page of orders."""
         r = self._robin_get("/orders/")
+        self._assert_200(r)
         # get only the filled orders
         orders = [x for x in json.loads(r.text)["results"] if x["state"] == "filled"]
         for x in orders:
@@ -45,8 +44,7 @@ class Robinhood:
             instrument = x["instrument"]
             if instrument not in self._cache:
                 instresp = self._robin_get(instrument)
-                if instresp.status_code != 200:
-                    raise Exception("HTTP %d: %s" % (self._cache[instrument].status_code, self._cache[instrument].text))
+                self._assert_200(instresp)
                 self._cache[instrument] = json.loads(instresp.text)
             x["instrument"] = self._cache[instrument]
         # clear the instrument cache afterward because they could change before the next call
@@ -55,14 +53,12 @@ class Robinhood:
     def get_portfolio(self):
         """Get the user's portfolio information."""
         r = self._robin_get("/portfolios/%s/" % (self._accounts["results"][0]["account_number"]))
-        if r.status_code != 200:
-            raise Exception("HTTP %d: %s" % (r.status_code, r.text))
+        self._assert_200(r)
         return json.loads(r.text)
     def get_portfolio_historicals(self, interval):
         """Get the user's historical portfolio performance."""
         r = self._robin_get("/portfolios/historicals/%s/?interval=%s" % (self._accounts["results"][0]["account_number"], interval))
-        if r.status_code != 200:
-            raise Exception("HTTP %d: %s" % (r.status_code, r.text))
+        self._assert_200(r)
         return json.loads(r.text)
 
 username = ""
